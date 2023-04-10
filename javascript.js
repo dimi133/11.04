e5be6f4199dee273893bdfffb46037b0b7e1c339
mikki133 <123013157+mikki133@users.noreply.github.com>
2023-04-10 09:31:03 +0200

    first commit

---
+++ javascript.js
@@ -0,0 +1,92 @@
+// 1
+// let d = 7;
+// console.log(d);
+// console.log(d**2);
+// console.log(d**3); 
+
+// 2 
+// let num = Number(prompt('Enter the Number'));
+// if(num > 0){
+//     let num1 = num*2;  //console.log(num*2);
+//     console.log(num1);
+// }
+// else{
+//     console.log('The number is negative')
+// }
+
+// 3
+// let num1 = Number(prompt('Enter the number 1 > '));
+// let num2 = Number(prompt('Enter the number 2 > '));
+// if(num1 > num2){
+//     let res1 = num1 + num2;
+//     console.log(res1);
+// }
+// else if(num2 > num1){
+//     console.log(num2 * num1);
+// }
+// else{
+//     console.log('The numbers are the same')
+// }
+
+// 4
+// let num = Number(prompt('Enter the number > '));
+// if(num < 0){
+//     console.log(num**2);
+// }
+// else{
+//     console.log('ERROR!!!');
+// }
+
+// 5 
+// let student = Number(prompt('Enter the number of student > '));
+// let chairs = Number(prompt('Enter the number of chairs > '));
+// if(student <= chairs){
+//     console.log('Enought chairs');
+// }
+// else{
+//     console.log('Not enought chairs');
+// }
+
+// 6
+// console.log('START > ');
+// for(let i=0; i<10; i++){
+//     console.log(i); 
+// }
+// console.log('< FINISH')
+
+
+// let num = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// console.log('START > ' + num + ' < FINISH');
+
+// 7
+// let array = [3, 15, -25, 25, 23, 75, 96, -1, 60, 17, 55, 40, 100];
+//  for( let i=0; i<array.length; i++){
+//     if(array[i]>=0 && array[i]%5==0 && array[i]<100)
+//     console.log(array[i]);
+//  }
+
+// 8
+// let array = [1, 7, 15, 32, 16, 9, 45, 13, 79, 5, 2, 16];
+// for( let i=0; i<array.length; i++){
+//     if(array[i]>=15){
+//         console.log(array[i])
+//     }
+// }
+
+// 9
+// let array = [12, 1, 17, 4, 34, 21, 67, 88, 52, 91, 3]
+// for(let i=0; i<array.length; i++){
+//     if(array[i]%2==1){
+//         console.log(array[i]);
+//     }
+// }
+    
+// 10
+// let array1 = [12, 3, 43, 17, 54, 79, 99, 41, 15, 23];
+// let array2 = [];
+// for(i = 0; i < array1.length; i++){
+//     if(i%3==0){
+//         array2.push(i);
+//         }
+//     }
+// console.log(array2);
